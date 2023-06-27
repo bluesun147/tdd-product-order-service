9492a5cf38402a4c4ef55cb982156308e2449057
bluesun147 <bluesun147@naver.com>
2023-06-27 22:53:28 +0900

    [Test] 상품주문 API 테스트 JPA 적용

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,24 +1,35 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
-    private final Product product;
-    private final int quantity;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         this.product = product;
         this.quantity = quantity;
         Assert.notNull(product, "상품은 필수입니다.");
     }
-
-    public void assignId(final Long aLong) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+//    public void assignId(final Long aLong) {
+//        this.id = id;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
 }
