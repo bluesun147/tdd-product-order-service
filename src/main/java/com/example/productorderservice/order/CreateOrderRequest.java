d639dbf7b772f46e155926acd009a3d57ab1a896
bluesun147 <bluesun147@naver.com>
2023-06-27 22:42:46 +0900

    [Refactor] 상품주문 API 테스트로 전환

--- src/main/java/com/example/productorderservice/order/CreateOrderRequest.java
+++ src/main/java/com/example/productorderservice/order/CreateOrderRequest.java
@@ -2,10 +2,8 @@ package com.example.productorderservice.order;
 
 import org.springframework.util.Assert;
 
-record CreateOrderRequest(Long productId, int quantity) {
-    CreateOrderRequest(final Long productId, final int quantity) {
-        this.productId = productId;
-        this.quantity = quantity;
+public record CreateOrderRequest(Long productId, int quantity) {
+    public CreateOrderRequest {
         Assert.notNull(productId, "상품 id는 필수입니다.");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
     }
